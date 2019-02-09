b18a03a63368714298bd20f257156debd6ad1f22
vrom911 <vrom911@gmail.com>
2019-02-10 01:43:31 +0800

    Fix jquery

--- js/main.js
+++ js/main.js
@@ -1,44 +1,44 @@
 //$("#overlay").height($(window.height()));
-$(window).load(function(){
-   // PAGE IS FULLY LOADED  
+$(window).on("load", function() {
+   // PAGE IS FULLY LOADED
    // FADE OUT YOUR OVERLAYING DIV
    $('.overlay').fadeOut();
 });
 $(document).ready(function(){
-	var h = $("#link").width();
-	$(".nav-parent").height(Math.floor(h/4));
-	$("#coming-canvas").height($("#coming-canvas").width()/2);
-	$(".icons").height($(".icon").width());
-	
-	
-	$(".icon").on("mouseenter",function(){
-		$(this).toggleClass("highlighted");
-	});
-	$(".icon").on("mouseleave",function(){
-		$(this).toggleClass("highlighted");
-	});
+  var h = $("#link").width();
+  $(".nav-parent").height(Math.floor(h/4));
+  $("#coming-canvas").height($("#coming-canvas").width()/2);
+  $(".icons").height($(".icon").width());
+
+
+  $(".icon").on("mouseenter",function(){
+    $(this).toggleClass("highlighted");
+  });
+  $(".icon").on("mouseleave",function(){
+    $(this).toggleClass("highlighted");
+  });
 
 });
 
 function resizeAll(){
-	var h = $("#link").width();
-	$(".nav-parent").height(Math.floor(h/4));
-	Processing.getInstanceById('logo-can').resizeLogo();
-	Processing.getInstanceById('coming-canvas').resizeComing();
-	Processing.getInstanceById('coming-canvas').draw();
-	Processing.getInstanceById('uz').resizeUzor();
-	Processing.getInstanceById('uz').draw();
-	Processing.getInstanceById('subuz').resizeUzor();
-	Processing.getInstanceById('subuz').draw();
-	Processing.getInstanceById('nav1').resizeNav();
-	Processing.getInstanceById('nav2').resizeNav();
-	Processing.getInstanceById('nav3').resizeNav();
-	Processing.getInstanceById('nav4').resizeNav();
+  var h = $("#link").width();
+  $(".nav-parent").height(Math.floor(h/4));
+  Processing.getInstanceById('logo-can').resizeLogo();
+  Processing.getInstanceById('coming-canvas').resizeComing();
+  Processing.getInstanceById('coming-canvas').draw();
+  Processing.getInstanceById('uz').resizeUzor();
+  Processing.getInstanceById('uz').draw();
+  Processing.getInstanceById('subuz').resizeUzor();
+  Processing.getInstanceById('subuz').draw();
+  Processing.getInstanceById('nav1').resizeNav();
+  Processing.getInstanceById('nav2').resizeNav();
+  Processing.getInstanceById('nav3').resizeNav();
+  Processing.getInstanceById('nav4').resizeNav();
 }
 $(window).resize(function() {
-	resizeAll();  
+  resizeAll();
 });
-// $(window).bind('resize', function () { 
-// 	$("#names").css({"fontSize": $("#name").width()/4});
-// 	$("#subname").css({"fontSize": $("#subname").height()/4});
+// $(window).bind('resize', function () {
+//  $("#names").css({"fontSize": $("#name").width()/4});
+//  $("#subname").css({"fontSize": $("#subname").height()/4});
 // });
