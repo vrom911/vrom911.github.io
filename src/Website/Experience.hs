c26c5cd2a326f7023f04a87c5aef7fe6f22e13db
vrom911 <vrom911@gmail.com>
2019-03-01 11:45:50 +0800

    Add more info

--- src/Website/Experience.hs
+++ src/Website/Experience.hs
@@ -11,6 +11,7 @@ data Experience = Experience
     , eImg      :: String
     , eTime     :: String
     , ePosition :: String
+    , eCountry  :: String
     }
 
 experienceCtx :: Context Experience
@@ -20,6 +21,7 @@ experienceCtx =
  <> field "eImg"      (pure . eImg      . itemBody)
  <> field "eTime"     (pure . eTime     . itemBody)
  <> field "ePosition" (pure . ePosition . itemBody)
+ <> field "eCountry"  (pure . eCountry  . itemBody)
 
 allExperience :: Compiler [Item Experience]
 allExperience = traverse makeItem
@@ -28,8 +30,42 @@ allExperience = traverse makeItem
         , eLink = "https://holmusk.com"
         , eImg  = "hm.jpg"
         , eTime = "May, 2018 — present"
-        , ePosition = "Haskell Developer"
+        , ePosition = "Middle Haskell Developer"
+        , eCountry = "Singapore"
         }
+    , Experience
+        { eName = "Serokell"
+        , eLink = "https://serokell.io"
+        , eImg  = "sk.png"
+        , eTime = "Nov, 2017 — April, 2018"
+        , ePosition = "Haskell Developer"
+        , eCountry = "Russia, Saint Petersburg"
+        }
+    , Experience
+        { eName = "Aelve"
+        , eLink = "https://aelve.com"
+        , eImg  = "ae.png"
+        , eTime = "Jul, 2017 — Oct, 2018"
+        , ePosition = "Junior Haskell Developer"
+        , eCountry = "Russia, Saint Petersburg"
+        }
+    , Experience
+        { eName = "EUG2016"
+        , eLink = "http://eug2016.com/"
+        , eImg  = "eug.jpg"
+        , eTime = "Jul, 2016 — Aug, 2016"
+        , ePosition = "Level 2 Helpdesk Engineer"
+        , eCountry = "Croatia, Zagreb"
+        }
+    , Experience
+        { eName = "ATEF"
+        , eLink = "http://www.atefgroup.com/en/"
+        , eImg  = "atef.png"
+        , eTime = "Jan, 2016 — Jul, 2016"
+        , ePosition = "Python Web Developer"
+        , eCountry = "Azerbaijan, Baku"
+        }
+
     ]
 
 mkExperienceCtx :: Context a
